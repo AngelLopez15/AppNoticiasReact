7d10287dc5a0529f91761eca3e23e2d114542c62
Angel Octavio Lopez <a.o.lopez.cruz@gmail.com>
2020-07-30 01:47:06 -0500

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# Buscador de Noticias
-
-## App hecha en React para buscar noticias
-
-App hecha para practicar peticiones a una API.
---
+++ precache-manifest.e19215c66377dc57a70181130bbc5037.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43abad0c29503eb94ba9c5ae28ab42f6",
+    "url": "/AppNoticiasReact/index.html"
+  },
+  {
+    "revision": "2ef8efe6b7e67f89c91e",
+    "url": "/AppNoticiasReact/static/css/main.ca4d5737.chunk.css"
+  },
+  {
+    "revision": "455f1c8a4ab8760d8e2e",
+    "url": "/AppNoticiasReact/static/js/2.d179136c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AppNoticiasReact/static/js/2.d179136c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ef8efe6b7e67f89c91e",
+    "url": "/AppNoticiasReact/static/js/main.517554bd.chunk.js"
+  },
+  {
+    "revision": "c153ca5533bcc428c5de",
+    "url": "/AppNoticiasReact/static/js/runtime-main.9fd2fc5b.js"
+  }
+]);
